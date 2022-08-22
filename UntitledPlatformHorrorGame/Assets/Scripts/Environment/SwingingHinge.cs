ae3268b88b825d0dd40ca893d0e76a3eeb02783c
Filip Golaboski <filipgolaboski@gmail.com>
2022-08-22 16:46:20 +0200

    Created SwingingHinge script

---
+++ UntitledPlatformHorrorGame/Assets/Scripts/Environment/SwingingHinge.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Envrionment
+{
+
+    public class SwingingHinge : MonoBehaviour
+    {
+        public float maxMotorSpeed;
+        public AnimationCurve motorSpeedCurve;
+        public HingeJoint2D HingeJoint2D;
+
+        float currentTargetMotorSpeed;
+        float currentTargetAngle;
+        JointMotor2D motor;
+
+        private void Start()
+        {
+            currentTargetMotorSpeed = maxMotorSpeed;
+            currentTargetAngle = HingeJoint2D.limits.max;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (HingeJoint2D.limits.max - HingeJoint2D.jointAngle < 0.1)
+            {
+                currentTargetMotorSpeed = -maxMotorSpeed;
+                currentTargetAngle = HingeJoint2D.limits.min;
+            }
+
+            if (HingeJoint2D.limits.min - HingeJoint2D.jointAngle > -0.1)
+            {
+                currentTargetMotorSpeed = maxMotorSpeed;
+                currentTargetAngle = HingeJoint2D.limits.max;
+            }
+
+            motor = HingeJoint2D.motor;
+            motor.motorSpeed = motorSpeedCurve.Evaluate(1 - Mathf.Abs(currentTargetAngle - HingeJoint2D.jointAngle) / (HingeJoint2D.limits.max + Mathf.Abs(HingeJoint2D.limits.min))) * currentTargetMotorSpeed;
+            HingeJoint2D.motor = motor;
+
+        }
+    }
+
+}
