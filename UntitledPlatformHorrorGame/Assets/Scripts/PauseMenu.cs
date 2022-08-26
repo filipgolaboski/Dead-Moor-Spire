a6565688c4383443b4c920258f6a5633ba15a3e8
AlenForko <alen.forko95@gmail.com>
2022-08-26 20:46:58 +0200

    PauseMenu + script

--- UntitledPlatformHorrorGame/Assets/Scripts/PauseMenu.cs
+++ UntitledPlatformHorrorGame/Assets/Scripts/PauseMenu.cs
@@ -1,18 +1,47 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PauseMenu : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public static bool gameIsPaused = false;
+
+    public GameObject pauseMenuUI;
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (gameIsPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        gameIsPaused = false;
+    }
+
+    void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        gameIsPaused = true;
+    }
+
+    public void QutiGame()
+    {
+        Application.Quit();
+        Debug.Log("Quitting game.");
     }
 }
